0745b76de995ac65dd1e03bc98950c9fd19cdd7e
k <Kylel@email.arizona.edu>
2023-11-25 23:22:24 -0700

    todo's only : pre-beta

---
+++ main.c
@@ -0,0 +1,13 @@
+
+
+//Prompt user for title for data and output the title
+
+//Prompt the user for the headers of two columns of a table and output column headers
+
+//Prompt the user for data points in the format string, 
+
+//check for entry has no comma, more than one comma, entry after the comma not integer
+
+//Output in formatted table
+
+//Output in formatted histogram
