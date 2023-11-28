0e98a0032db585816888942436d3b44363df8486
k <Kylel@email.arizona.edu>
2023-11-27 18:18:29 -0700

    visualization functional with minor bugs ie dont add spaces after the 'row name, integer' lest segfault and memory problems haunt you

--- main.c
+++ main.c
@@ -8,20 +8,41 @@ typedef struct data_node_struct{
    int col2_dat;
    struct data_node_struct * nextNodePtr;
 }data_node;
-
+char * itoa(int i);
+void PrintTableHeader(char *title, char *col1, char *col2);
+void PrintTableRow(char *col1, int col2);
+void PrintTable(data_node *head, char *title, char *col1, char *col2){
+   data_node *temp=head;
+   PrintTableHeader(title, col1, col2);
+   while(temp->nextNodePtr!=NULL){
+      PrintTableRow(temp->col1_dat, temp->col2_dat);
+      temp = temp->nextNodePtr;
+   }PrintTableRow(temp->col1_dat, temp->col2_dat);
+   return;
+}
+void PrintHistRow(char *col1, int col2);
+void PrintHist(data_node *head, char *title, char *col1, char *col2){
+   data_node *temp=head;
+   while(temp->nextNodePtr!=NULL){
+      PrintHistRow(temp->col1_dat, temp->col2_dat);
+      temp = temp->nextNodePtr;
+   }PrintHistRow(temp->col1_dat, temp->col2_dat);
+   return;
+}
 void remove_new_line(char *s);
 data_node * build_data_list(void);
 void FreeDataList(data_node* head);
+
 int main(void){
    char dataTitle[MAX_LEN], col1Header[MAX_LEN], col2Header[MAX_LEN];
    data_node * headObj;
 //Prompt user for title for data and output the title
-   /*
+   
    printf("Enter a title for the data:\n");
    fgets(dataTitle, MAX_LEN, stdin);
    remove_new_line(dataTitle);
-   printf("You entered: %s", dataTitle);
-   */
+   printf("You entered: %s\n", dataTitle);
+   
 //Prompt the user for the headers of two columns of a table and output column headers
    
    printf("Enter the column 1 header:\n");
@@ -34,17 +55,18 @@ int main(void){
    printf("You entered: %s\n", col2Header);
    
 //Prompt the user for data points in the format string, 
-
 //check for entry has no comma, more than one comma, entry after the comma not integer
    headObj = build_data_list();
-
    
 //Output in formatted table
-
+   PrintTable(headObj, dataTitle, col1Header, col2Header);
+   
 //Output in formatted histogram
-
+   PrintHist(headObj, dataTitle, col1Header, col2Header);
+   
 //clean up and return from main
    FreeDataList(headObj);
+   
   return 0;
 }
 void remove_new_line(char *s){
@@ -103,13 +125,6 @@ int parse_comma_separated_data(char *s, int *comma_loc){
    int i, n=strlen(s);
    if(no_commas(s)!=1){return -1;}
    for(i=0; i<n; i++){
-      /*if(!( (s[i]>=65&&s[i]<=90) || (s[i]>=97&&s[i]<=122) || (s[i]>=48 &&s[i]<=57) || (s[i]==' ') ) ){//find the next alpha
-         *comma_loc = i;
-         while(!( (s[i]>=65&&s[i]<=90) || (s[i]>=97&&s[i]<=122) || (s[i]>=48 &&s[i]<=57) || (s[i]==' ') ) ){// means s_i is not alphanumeric space
-            i++;
-         }
-         break;
-      }*/
       if(s[i] == ','){*comma_loc=i;return i;}
    }
    return -1;
@@ -120,6 +135,7 @@ void parse(char *buf, int *l, int *c, char *first, char *last){
    remove_new_line(first);
    remove_spaces(first);
    strncpy(last, buf, *l);
+   last[*l] = '\0';
    return;
 }
 void InsertAfter(data_node * thisNode, data_node * newNode){
@@ -160,7 +176,7 @@ data_node * build_data_list(void) {
    data_node *headObj, *temp;
    char first[MAX_LEN], last[MAX_LEN];
    char buf[MAX_LEN]="";
-   int i_l, i_c, k=0;
+   int i_l, i_c, list_size=0;
    
    while(strcmp(buf, "-1\n")!=0){
       
@@ -185,7 +201,7 @@ data_node * build_data_list(void) {
       printf("Data string: %s\n", last);
       printf("Data integer: %s\n\n", first);
       
-      if(k==0){
+      if(list_size==0){
          headObj = (data_node *)malloc(sizeof(data_node));
          strcpy(headObj->col1_dat, last);
          headObj->col2_dat = atoi(first);
@@ -193,11 +209,67 @@ data_node * build_data_list(void) {
       }
       else{//add a new one
          temp = (data_node *)malloc(sizeof(data_node));
+         strcpy(temp->col1_dat, last);
+         temp->col2_dat = atoi(first);
          temp->nextNodePtr=NULL;
          InsertAfter(getLast(headObj), temp);
       }
+      list_size++;
       
    }
    
    return headObj;
 }
+
+void PrintTableHeader(char *title, char *col1, char *col2){
+   int i, middex= strlen(title)/2, number_spaces_preceding_title;
+   number_spaces_preceding_title = 21 - middex;
+   for(i=0;i<number_spaces_preceding_title; i++){printf(" ");}
+   printf("%s\n", title);
+   printf("%-20s|", col1);
+   printf("%23s\n", col2);
+   for(i=0; i<44;i++){printf("-");}
+   printf("\n");
+   return;
+}
+void PrintTableRow(char *col1, int col2){
+   char *COL2 = itoa(col2);
+   printf("%-20s|", col1);
+   printf("%23s\n", COL2);
+   free(COL2);
+   return;
+}
+void PrintHistRow(char *col1, int col2){
+   int i;
+   printf("%20s ", col1);
+   for(i=0; i<col2; i++){printf("*");}
+   printf("\n");
+   return;
+}
+int power(int base, int exponent){
+    if (exponent == 0){
+        return 1; }
+    else{
+        return (base * power(base, exponent - 1)); }
+}
+char * itoa(int i){
+   int num=i, e=0, j=0, k=0, h=0;
+   char buf[10]={'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
+   char *s = (char*)malloc(sizeof(char)*10);
+   for(h=0; h<10; h++){s[h] = '\0';}
+   while(num!=0){
+      buf[9-e] = num % power(10, e) + 48;//because ascii 0 is 48
+      num = (num - num % power(10, e))/power(10, e);
+      e++;
+   }
+   // make the array a string
+   while(buf[9-j]!='\0'){j++;}j--;
+   while(j!=0){
+      s[k] = buf[9-j];
+      k++;j--;
+   }
+   return s;
+}
+
+   
+
